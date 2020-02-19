0423743ad0a54f3ee4100a5abbd052b3b52035ba
Ankit Kumar <ankit_sony3@hotmail.com>
2020-02-19 16:00:17 +0530

    repo is not required for movies

--- src/main/java/com/bootcamp/dev/devcamp/repository/MoviesRepository.java
+++ src/main/java/com/bootcamp/dev/devcamp/repository/MoviesRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+// TODO: remove it, not in use
 
 @Repository
 public interface MoviesRepository extends ReactiveMongoRepository<Movie, String> {
